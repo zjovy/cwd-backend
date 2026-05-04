96bbe5aa53f2740878b2ce3231bc67e540e63b98
Jefferson Wu <jeffersonw888@gmail.com>
2026-05-03 21:50:51 -0500

    allow only admin to get users

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -15,7 +15,7 @@ router.post('/logout', authMiddleware, authController.logout);
 router.get('/me', authMiddleware, authController.getMe);
 router.post('/token', authLimiter, authController.handleToken);
 
-router.get('/users', authMiddleware, authController.getAllUsers);
+router.get('/users', authMiddleware, adminMiddleware, authController.getAllUsers);
 router.patch('/users/:uid/role', authMiddleware, adminMiddleware, authController.setRole);
 
 export default router;
